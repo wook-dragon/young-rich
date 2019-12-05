7299a13a8888d8ae02bd06926d81f8fc1376ea4a
ILGYEOM <chdl1229@gmail.com>
2019-12-05 11:35:41 +0900

    수정

--- Factory_Pattern/kum/patton/CarStore.java
+++ Factory_Pattern/kum/patton/CarStore.java
@@ -3,10 +3,10 @@ package patton;
 public class CarStore {
 
 	public static void main(String[] args) {
-		CarBrand brand = CarFactory2.createCar(CarNames.Kia);
+		CarBrand brand = CarFactory.createCar(CarNames.Kia);
 		System.out.println("차 이름 :" + brand.getName());
 	
-		CarBrand brand2 = CarFactory2.createCar(CarNames.Hyundai);
+		CarBrand brand2 = CarFactory.createCar(CarNames.Hyundai);
 		System.out.println("차 이름 :" + brand2.getName());
 	}
 
